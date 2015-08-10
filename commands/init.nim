1523803f1fb5e19738e78dde907bc97e4f6f3531
Andrew Vy <andrew@andrewvy.com>
2015-08-10 10:08:56 -0700

    Add config.toml generator

--- commands/init.nim
+++ commands/init.nim
@@ -7,6 +7,7 @@ proc init(args: Table) =
     var F_VERBOSE = false
     var CURRENT_DIR: string
     var project_file, project_name: string
+    var project_file_out: File
 
     if args["-v"]:
         F_VERBOSE = true
@@ -20,18 +21,27 @@ proc init(args: Table) =
     echo "Initializing new Nima project!"
 
     # Init project directory
-    project_file = project_name & ".nima"
+    project_file = "config.toml"
     CURRENT_DIR = os.getCurrentDir()
 
     try:
         if existsDir(CURRENT_DIR / project_name):
             raise newException(NimaError, "Directory already exists with that name: " & project_name)
-
         createDir(CURRENT_DIR / project_name)
-
     except NimaError:
         echo "ERROR: " & getCurrentExceptionMsg()
 
     # Init config files
 
-    echo args
+    try:
+        if existsFile(CURRENT_DIR / project_name / project_file):
+            raise newException(NimaError, "Nima project file already exists in project directory!")
+        if open(f=projectFileOut, filename = CURRENT_DIR/project_name/project_file, mode = fmWrite):
+            projectFileOut.writeln("title = \"" & project_name & "\"")
+            projectFileOut.writeln("version = \"0.1.0\"")
+            projectFileOut.writeln("author = \"Anonymous\"")
+            close(projectFileOut)
+        else:
+            raise newException(NimaError, "Failed to create .nima config file")
+    except NimaError:
+        echo "ERROR: " & getCurrentExceptionMsg()
