bdf0f54aa1dcc81143e9e090c9bd5959ecbf358d
Andrew Vy <andrew@andrewvy.com>
2015-08-10 18:15:41 -0700

    Remove seed files and add compile_html proc

--- commands/build.nim
+++ commands/build.nim
@@ -1,5 +1,21 @@
+import types
+
 import docopt
+import os
 import strutils
 
+proc compile_html(CURRENT_DIR: string) =
+    echo "Compiling HTML"
+
 proc build*(args: Table) =
-    echo "Building.."
+    let CURRENT_DIR = os.getCurrentDir()
+    let CONFIG_FILE = "config.toml"
+
+    try:
+        if existsFile(CURRENT_DIR/CONFIG_FILE):
+            echo "Building Nima project.."
+            compile_html(CURRENT_DIR)
+        else:
+            raise newException(NimaError, "Not currently in a Nima project!")
+    except NimaError:
+        echo "ERROR: " & getCurrentExceptionMsg()
